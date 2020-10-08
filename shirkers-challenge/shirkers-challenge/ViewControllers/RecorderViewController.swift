e8207bbb4794daec2099ede90fe4df932b09e2b9
csfar <csf.artur@gmail.com>
2020-10-08 10:32:15 -0300

    Add animation to recording button

--- shirkers-challenge/shirkers-challenge/ViewControllers/RecorderViewController.swift
+++ shirkers-challenge/shirkers-challenge/ViewControllers/RecorderViewController.swift
@@ -13,6 +13,16 @@ final class RecorderViewController: UIViewController {
     @AutoLayout private var recordButton: UIButton
     @AutoLayout private var timestampLabel: UILabel
 
+    private var recording: Bool = false
+
+    /// The anchors identifier used to animate constraints.
+    fileprivate enum AnchorIdentifier: String {
+        case recordButtonBottom = "record-bottom"
+        case recordButtonWidth = "record-width"
+        case recordButtonHeight = "record-height"
+        case timestampLabelBottom = "timestamp-bottom"
+    }
+
     // MARK: Init
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -32,7 +42,52 @@ final class RecorderViewController: UIViewController {
 
     // MARK: @objc
     @objc private func didTapRecord(_ button: UIButton) {
-        button.setBackgroundImage(UIImage(systemName: "smallcircle.fill.circle"), for: .normal)
+        guard let timestampBottomAnchor = view.constraints.first(where: {
+            $0.identifier == AnchorIdentifier.timestampLabelBottom.rawValue
+        }) else { return }
+        guard let bottomAnchor = self.view.constraints.first(where: {
+            $0.identifier == AnchorIdentifier.recordButtonBottom.rawValue
+        }) else { return }
+        guard let widthAnchor = button.constraints.first(where: {
+            $0.identifier == AnchorIdentifier.recordButtonWidth.rawValue
+        }) else { return }
+        guard let heightAnchor = button.constraints.first(where: {
+            $0.identifier == AnchorIdentifier.recordButtonHeight.rawValue
+        }) else { return }
+
+        if recording {
+            timestampBottomAnchor.constant = -(DesignSystem.Recorder.spacingFromRecordButton)
+
+            UIView.animate(withDuration: 0.2,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 1) { [weak self] in
+                guard let self = self else { return }
+                button.layer.cornerRadius = (DesignSystem.Recorder.recordButtonHeight * 1.0) / 2
+                bottomAnchor.constant = -(DesignSystem.Recorder.spacingFromBottom)
+                widthAnchor.constant = DesignSystem.Recorder.recordButtonWidth * 1.0
+                heightAnchor.constant = DesignSystem.Recorder.recordButtonHeight * 1.0
+
+                button.layoutIfNeeded()
+                self.recording = false
+            }
+        } else {
+            timestampBottomAnchor.constant = -(DesignSystem.Recorder.spacingFromRecordButton + DesignSystem.Recorder.recordButtonHeight * 0.5/2)
+
+            UIView.animate(withDuration: 0.2,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 1) { [weak self] in
+                guard let self = self else { return }
+                button.layer.cornerRadius = (DesignSystem.Recorder.recordButtonHeight * 0.5) / 2
+                bottomAnchor.constant = -(DesignSystem.Recorder.spacingFromBottom + (DesignSystem.Recorder.recordButtonHeight * 0.5)/2)
+                widthAnchor.constant = DesignSystem.Recorder.recordButtonWidth * 0.5
+                heightAnchor.constant = DesignSystem.Recorder.recordButtonHeight * 0.5
+
+                button.layoutIfNeeded()
+                self.recording = true
+            }
+        }
     }
 
     // MARK: Views setup
@@ -42,8 +97,9 @@ final class RecorderViewController: UIViewController {
     }
 
     private func setUpRecordButton() {
-        recordButton.setBackgroundImage(UIImage(systemName: "circle.fill"), for: .normal)
-        recordButton.tintColor = .memoraRed
+        recordButton.backgroundColor = .memoraRed
+        recordButton.layer.cornerRadius = DesignSystem.Recorder.recordButtonHeight/2
+        recordButton.clipsToBounds = true
 
         recordButton.addTarget(self, action: #selector(didTapRecord(_:)), for: .touchUpInside)
     }
@@ -66,13 +122,20 @@ final class RecorderViewController: UIViewController {
 
         let guides = view.safeAreaLayoutGuide
 
-        NSLayoutConstraint.activate([
-            recordButton.bottomAnchor.constraint(equalTo: guides.bottomAnchor,
-                                                 constant: -DesignSystem.Recorder.spacingFromBottom),
-            recordButton.widthAnchor.constraint(equalToConstant: DesignSystem.Recorder.recordButtonWidth),
-            recordButton.heightAnchor.constraint(equalToConstant: DesignSystem.Recorder.recordButtonHeight),
-            recordButton.centerXAnchor.constraint(equalTo: guides.centerXAnchor)
-        ])
+        let bottomConstraint = recordButton.bottomAnchor.constraint(equalTo: guides.bottomAnchor,
+                                             constant: -DesignSystem.Recorder.spacingFromBottom)
+        let widthConstraint = recordButton.widthAnchor.constraint(equalToConstant: DesignSystem.Recorder.recordButtonWidth)
+        let heightConstraint = recordButton.heightAnchor.constraint(equalToConstant: DesignSystem.Recorder.recordButtonHeight)
+        let centerXconstraint = recordButton.centerXAnchor.constraint(equalTo: guides.centerXAnchor)
+
+        NSLayoutConstraint.activate([bottomConstraint,
+                                     widthConstraint,
+                                     heightConstraint,
+                                     centerXconstraint])
+
+        widthConstraint.identifier = AnchorIdentifier.recordButtonWidth.rawValue
+        heightConstraint.identifier = AnchorIdentifier.recordButtonHeight.rawValue
+        bottomConstraint.identifier = AnchorIdentifier.recordButtonBottom.rawValue
     }
 
     private func layoutTimestampLabelConstraints() {
@@ -80,13 +143,17 @@ final class RecorderViewController: UIViewController {
 
         let guides = view.safeAreaLayoutGuide
 
+        let bottomAnchor = timestampLabel.bottomAnchor.constraint(equalTo: recordButton.topAnchor,
+                                                                  constant: -DesignSystem.Recorder.spacingFromRecordButton)
+
         NSLayoutConstraint.activate([
-            timestampLabel.bottomAnchor.constraint(equalTo: recordButton.topAnchor,
-                                                 constant: -DesignSystem.Recorder.spacingFromRecordButton),
+            bottomAnchor,
             timestampLabel.widthAnchor.constraint(equalTo: guides.widthAnchor,
-                                                multiplier: DesignSystem.Recorder.timestampLabelWidthMultiplier),
+                                                  multiplier: DesignSystem.Recorder.timestampLabelWidthMultiplier),
             timestampLabel.heightAnchor.constraint(equalToConstant: DesignSystem.Recorder.timestampLabelHeight),
             timestampLabel.centerXAnchor.constraint(equalTo: guides.centerXAnchor)
         ])
+
+        bottomAnchor.identifier = AnchorIdentifier.timestampLabelBottom.rawValue
     }
 }
