c745d75170eb27ea761460bd7d62283d206afc46
csfar <csf.artur@gmail.com>
2020-10-14 10:35:42 -0300

    [WIP] Add ArchiveViewModel with FRC

---
+++ shirkers-challenge/shirkers-challenge/ViewModels/ArchiveTableViewCellViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ArchiveTableViewCellViewModel.swift
+//  shirkers-challenge
+//
+//  Created by Artur Carneiro on 14/10/20.
+//  Copyright © 2020 Artur Carneiro. All rights reserved.
+//
+
+import Foundation
+
+struct ArchiveTableViewCellViewModel {
+    let title: String
+    let createdAt: String
+    let isActive: Bool
+    let modifiedAt: String
+    let period: String
+
+    init(title: String = "",
+         createdAt: String = "",
+         isActive: Bool = false,
+         modifiedAt: String = "",
+         period: String = "") {
+        self.title = title
+        self.createdAt = createdAt
+        self.isActive = isActive
+        self.modifiedAt = modifiedAt
+        self.period = period
+    }
+}
--- shirkers-challenge/shirkers-challenge/ViewModels/ArchiveViewModel.swift
+++ shirkers-challenge/shirkers-challenge/ViewModels/ArchiveViewModel.swift
@@ -8,5 +8,72 @@
 
 import CoreData
 
-final class ArchiveViewModel {
+protocol ArchiveViewModelDelegate: AnyObject {
+    func beginUpdate()
+    func insertNewEntryAt(_ index: IndexPath)
+    func endUpdate()
+}
+
+final class ArchiveViewModel: NSObject {
+    // MARK: - Properties
+    private let context: NSManagedObjectContext
+
+    private(set) lazy var fetchedResultsController: NSFetchedResultsController<Recording> = {
+        let fetchRequest: NSFetchRequest<Recording> = Recording.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Recording.modifiedAt, ascending: false)]
+
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                             managedObjectContext: self.context,
+                                             sectionNameKeyPath: nil,
+                                             cacheName: nil)
+        frc.delegate = self
+
+        return frc
+    }()
+
+    weak var delegate: ArchiveViewModelDelegate?
+
+    // MARK: - Init
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+
+    var numberOfMemories: Int {
+        guard let memories = fetchedResultsController.fetchedObjects else {
+            return 0
+        }
+        return memories.count
+    }
+
+    func requestFetch() {
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print(error.localizedDescription)
+            // TODO: Error handling
+        }
+    }
+
+    func viewModelAt(index: IndexPath) -> ArchiveTableViewCellViewModel {
+        let memory = fetchedResultsController.object(at: index)
+
+        guard let title = memory.title,
+              let createdAt = memory.createdAt?.stringFormatted(),
+              let modifiedAt = memory.modifiedAt?.stringFormatted(),
+              let period = memory.period?.stringFormatted() else {
+            return ArchiveTableViewCellViewModel()
+        }
+
+        return ArchiveTableViewCellViewModel(title: title,
+                                             createdAt: createdAt,
+                                             isActive: memory.isActive,
+                                             modifiedAt: modifiedAt,
+                                             period: period)
+    }
+
+}
+
+// MARK: - FRC Delegate
+extension ArchiveViewModel: NSFetchedResultsControllerDelegate {
+
 }
